7633d50145d56ce47d60d533d03652bb0b635772
Haresproject <kosthandoko907@gmail.com>
2026-07-20 14:42:00 +0700

    7

--- js/settings.js
+++ js/settings.js
@@ -148,3 +148,54 @@ function applyTheme(theme){
         .setProperty("--accent", c.accent);
 
 }
+// ================= PREVIEW LOGO =================
+
+document
+.getElementById("logoFile")
+.addEventListener("change",function(){
+
+    const file=this.files[0];
+
+    if(!file) return;
+
+    const reader=new FileReader();
+
+    reader.onload=function(e){
+
+        const img=document.getElementById("logoPreview");
+
+        img.src=e.target.result;
+
+        img.style.display="block";
+
+    };
+
+    reader.readAsDataURL(file);
+
+});
+
+// ================= PREVIEW BACKGROUND =================
+
+document
+.getElementById("bgFile")
+.addEventListener("change",function(){
+
+    const file=this.files[0];
+
+    if(!file) return;
+
+    const reader=new FileReader();
+
+    reader.onload=function(e){
+
+        const img=document.getElementById("bgPreview");
+
+        img.src=e.target.result;
+
+        img.style.display="block";
+
+    };
+
+    reader.readAsDataURL(file);
+
+});
